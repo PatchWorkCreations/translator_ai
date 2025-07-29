44afe83cf81a5d93453287e1128c40d12b0b62e8
Jvictorio09 <juliavictorio16@gmail.com>
2025-07-29 22:30:24 +0800

    udpated

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-start: gunicorn myProject.wsgi:application --bind 0.0.0.0:8080
+web: gunicorn myProject.wsgi:application --bind 0.0.0.0:8080
--- myProject/settings.py
+++ myProject/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myProject.urls'
@@ -112,7 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
